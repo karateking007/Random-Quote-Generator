3cc01b67af71a2f111aa917da41b1ca97eb3b37c
Riley <rileylitchfield95@gmail.com>
2022-09-05 18:30:48 -0500

    Removed old code

--- app.js
+++ app.js
@@ -57,41 +57,6 @@ function loadQuote() {
     }, 500);
 }
 
-
-// function loadQuote() {
-//     setTimeout(function () {
-//         $.ajax({
-//             url: 'https://quotable.io/random',
-//             dataType: "json",
-//             success: function (data) {
-//                 if(data["length"] > 120) {
-//                     loadQuote();
-//                 } else {
-//                     changeColors();
-
-//                     $("#twitter-icon").addClass("fa-twitter-square");
-
-//                     quoteText = '"' + data["content"] + '"';
-//                     $("#text").html(quoteText);
-//                     quoteAuthor = data["author"];
-//                     $("#author").html(quoteAuthor);
-
-//                     $("h2,h4,.btn,.fab").fadeIn(500);
-
-//                     var quoteSplitStr = quoteText.split(" ");
-//                     var quoteUrlString = quoteSplitStr.join("%20");
-//                     var authorSplitStr = quoteAuthor.split(" ");
-//                     var authorUrlString = authorSplitStr.join("%20");
-
-//                     var twitterURL = "https://twitter.com/intent/tweet?text=";
-//                     var result = twitterURL + quoteUrlString + " -" + authorUrlString;
-//                     $("a").attr("href", result);
-//                 }
-//             }
-//         });
-//     }, 500);
-// }
-
 $("#new-quote").click(function () {
     $("h2,h4,.btn,.fab").fadeOut(500);
 
