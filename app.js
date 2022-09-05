66e3ecdd1d04545e2a47998181263e5ab4dcefba
Riley <rileylitchfield95@gmail.com>
2022-09-05 13:32:14 -0500

    Upgraded to api-ninjas API

--- app.js
+++ app.js
@@ -24,36 +24,71 @@ function changeColors() {
 function loadQuote() {
     setTimeout(function () {
         $.ajax({
-            url: 'https://quotable.io/random',
-            dataType: "json",
+            method: 'GET',
+            url: 'https://api.api-ninjas.com/v1/quotes',
+            headers: { 'X-Api-Key': API_KEY },
+            contentType: 'application/json',
             success: function (data) {
-                if(data["length"] > 120) {
+                if (data[0]["quote"].length > 120) {
                     loadQuote();
                 } else {
                     changeColors();
 
                     $("#twitter-icon").addClass("fa-twitter-square");
 
-                    quoteText = '"' + data["content"] + '"';
+                    quoteText = data[0]["quote"];
                     $("#text").html(quoteText);
-                    quoteAuthor = data["author"];
+                    quoteAuthor = data[0]["author"];
                     $("#author").html(quoteAuthor);
 
                     $("h2,h4,.btn,.fab").fadeIn(500);
 
-                    var quoteSplitStr = quoteText.split(" ");
-                    var quoteUrlString = quoteSplitStr.join("%20");
-                    var authorSplitStr = quoteAuthor.split(" ");
-                    var authorUrlString = authorSplitStr.join("%20");
-
                     var twitterURL = "https://twitter.com/intent/tweet?text=";
-                    var result = twitterURL + quoteUrlString + " -" + authorUrlString;
+                    var result = twitterURL + quoteText + " - " + quoteAuthor;
                     $("a").attr("href", result);
                 }
+            },
+            error: function ajaxError(jqXHR) {
+                console.error('Error: ', jqXHR.responseText);
             }
         });
     }, 500);
 }
+
+
+// function loadQuote() {
+//     setTimeout(function () {
+//         $.ajax({
+//             url: 'https://quotable.io/random',
+//             dataType: "json",
+//             success: function (data) {
+//                 if(data["length"] > 120) {
+//                     loadQuote();
+//                 } else {
+//                     changeColors();
+
+//                     $("#twitter-icon").addClass("fa-twitter-square");
+
+//                     quoteText = '"' + data["content"] + '"';
+//                     $("#text").html(quoteText);
+//                     quoteAuthor = data["author"];
+//                     $("#author").html(quoteAuthor);
+
+//                     $("h2,h4,.btn,.fab").fadeIn(500);
+
+//                     var quoteSplitStr = quoteText.split(" ");
+//                     var quoteUrlString = quoteSplitStr.join("%20");
+//                     var authorSplitStr = quoteAuthor.split(" ");
+//                     var authorUrlString = authorSplitStr.join("%20");
+
+//                     var twitterURL = "https://twitter.com/intent/tweet?text=";
+//                     var result = twitterURL + quoteUrlString + " -" + authorUrlString;
+//                     $("a").attr("href", result);
+//                 }
+//             }
+//         });
+//     }, 500);
+// }
 
 $("#new-quote").click(function () {
     $("h2,h4,.btn,.fab").fadeOut(500);
