3526df8756569563f1d928309091a602ef35a2e2
Riley <rileylitchfield95@gmail.com>
2022-09-13 17:09:55 -0500

    Added SASS package to project

--- src/App.js
+++ src/App.js
@@ -4,26 +4,25 @@ import './App.css';
 import QuoteBlock from './components/QuoteBlock';
 
 
-function App() {
-  return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <QuoteBlock />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: ''
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <QuoteBlock />
+      </div>
+    );
+  }
 }
 
 export default App;
