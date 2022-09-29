72545c2a6088d423263f37cd7b405bfffeada494
Riley <rileylitchfield95@gmail.com>
2022-09-28 20:25:34 -0500

    Updated styling

--- src/components/QuoteBlock.js
+++ src/components/QuoteBlock.js
@@ -9,19 +9,19 @@ class QuoteBlock extends Component {
     render() {
         return (
             <div className="container-fluid" style={{ backgroundColor: this.props.bgColor }}>
-                <div className="card d-flex align-items-center w-100 p-4">
+                <div className="card d-flex align-items-center w-100 p-3 p-sm-4">
                     <div className="row flex-grow-1">
                         <div className="col text-center align-self-center">
                             <div className="row">
                                 <div className="col text-center">
-                                    <p id="text" className="text-center fs-5 transition-color" style={{ color: this.props.bgColor }}>
+                                    <p id="quote" className="text-center fs-3 fw-bold transition-color" style={{ color: this.props.bgColor }}>
                                         {this.props.loading == false ? this.props.apiData.quote : <a target="_blank" href="https://icons8.com/icon/H6C79JoP90DH/settings"><img src={load} style={{ width: 100 }} /></a>}
                                     </p>
                                 </div>
                             </div>
                             <div className="row">
                                 <div className="col text-center">
-                                    <p id="author" className="text-center fs-6 fw-light transition-color" style={{ color: this.props.bgColor }}>
+                                    <p id="author" className="text-center fs-5 transition-color" style={{ color: this.props.bgColor }}>
                                         {this.props.loading == false ? `- ${this.props.apiData.author}` : null}
                                     </p>
                                 </div>
